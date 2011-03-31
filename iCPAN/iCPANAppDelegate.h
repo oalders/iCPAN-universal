2ba6d6250be2cb54ee971af5293ee6fde12c6c73
Olaf Alders <olaf@wundersolutions.com>
2011-03-31 00:20:25 -0400

    Initial Commit

---
+++ iCPAN/iCPANAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  iCPANAppDelegate.h
+//  iCPAN
+//
+//  Created by Olaf Alders on 11-03-31.
+//  Copyright 2011 wundersolutions.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCPANAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iCPAN/iPad/iCPANAppDelegate_iPad.m
@@ -0,0 +1,18 @@
+//
+//  iCPANAppDelegate_iPad.m
+//  iCPAN
+//
+//  Created by Olaf Alders on 11-03-31.
+//  Copyright 2011 wundersolutions.com. All rights reserved.
+//
+
+#import "iCPANAppDelegate_iPad.h"
+
+@implementation iCPANAppDelegate_iPad
+
+- (void)dealloc
+{
+	[super dealloc];
+}
+
+@end
