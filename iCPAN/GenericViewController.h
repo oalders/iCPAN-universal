0b55d15532e00a589e013e5d2add15ff43d40e1e
Olaf Alders <olaf@wundersolutions.com>
2011-05-18 00:41:11 -0400

    Removes RootViewController in favour of custom GenericViewController in iPad

---
+++ iCPAN/GenericViewController.h
@@ -0,0 +1,16 @@
+//
+//  GenericViewController.h
+//  iCPAN
+//
+//  Created by Olaf Alders on 11-05-18.
+//  Copyright 2011 wundersolutions.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GenericViewController : UIViewController {
+    
+}
+
+@end
